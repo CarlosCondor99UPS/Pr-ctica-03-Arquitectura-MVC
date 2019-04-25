383faf2e2f7e8f91b3ca30a48ef9be4a88e1fe3e
tians <tians@DESKTOP-JKU2HJ4>
2019-04-25 08:50:47 -0500

    Al menos una clase Controlador debe ser implementada usando la interface List y la clase ArrayList.

--- src/ec/edu/ups/controlador/ControladorArtropodo.java
+++ src/ec/edu/ups/controlador/ControladorArtropodo.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.ups.controlador;
 
 import ec.edu.ups.modelo.Artropodo;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author tians
- */
 public class ControladorArtropodo {
     private List<Artropodo> artropodos= new ArrayList<>();;
 
