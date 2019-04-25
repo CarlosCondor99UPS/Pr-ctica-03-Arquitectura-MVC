81df5237e1bdb8244d5847fd0097b1b8dd8e3c5a
tians <tians@DESKTOP-JKU2HJ4>
2019-04-25 09:00:17 -0500

    Al menos una clase Controlador debe ser implementada usando la interface Map y la clase HashMap o la interface Map y la clase TreeMap.

--- src/ec/edu/ups/controlador/ControladorPez.java
+++ src/ec/edu/ups/controlador/ControladorPez.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.ups.controlador;
 
 import ec.edu.ups.modelo.Pez;
 import java.util.Map;
 import java.util.TreeMap;
 
-/**
- *
- * @author tians
- */
 public class ControladorPez {
 
     private Map<Integer, Pez> peces;
